7d6f03deabc8a04f70bd4725370472710a54f144
Aleksandr <skurixxx03@gmail.com>
2021-10-19 04:29:55 +0300

    Лаб 5.2

---
+++ Lab5.2/Lab5.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+double a, b, c, ac, bc, sum;
+int main()
+{
+	cin >> a >> b >> c;
+	ac = abs(a - c);
+	bc = abs(b - c);
+	sum = ac + bc;
+
+	cout << "AC = " << ac << endl;
+	cout << "BC = " << bc << endl;
+	cout << "AC + BC = " << sum << endl;
+	return 0;
+}
