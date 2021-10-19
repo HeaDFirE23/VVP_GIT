d3150681c28c12b7c18248d0afd774e2fbd36320
Aleksandr <skurixxx03@gmail.com>
2021-10-19 04:20:08 +0300

    Лаб 4.5

---
+++ Lab4.5/Lab4.5.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+double a, b, sum, raz, pr, ch;
+int main()
+{
+	cin >> a >> b;
+	a = abs(a);
+	b = abs(b);
+	sum = a + b;
+	raz = a - b;
+	pr = a * b;
+	ch = a / b;
+
+	cout << "Sum = " << sum << endl;
+	cout << "Raz = " << raz << endl;
+	cout << "Pr = " << pr << endl;
+	cout << "Chas = " << ch << endl;
+	return 0;
+}
