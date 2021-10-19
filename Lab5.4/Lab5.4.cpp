c71ff2084701a1d5862b8320ab6861441797e7ca
Aleksandr <skurixxx03@gmail.com>
2021-10-19 04:44:14 +0300

    Лаб 5.4

---
+++ Lab5.4/Lab5.4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+double x, xx, y, yy, P, S;
+int main()
+{
+	cin >> x >> y >> xx >> yy;
+	P = 2 * (abs(xx - x) + abs(yy - y));
+	S = abs(xx - x) * abs(yy - y);
+
+	cout << "P = " << P << endl;
+	cout << "S = " << S << endl;
+	return 0;
+}
