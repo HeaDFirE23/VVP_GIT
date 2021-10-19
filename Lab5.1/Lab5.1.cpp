b8c1165ee555f3f5cb992839777a899aa6fc636d
Aleksandr <skurixxx03@gmail.com>
2021-10-19 04:22:21 +0300

    Лаб 5.1

---
+++ Lab5.1/Lab5.1.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+double x, xx, y, yy, l;
+int main()
+{
+	cin >> x >> y >> xx >> yy;
+	l = sqrt((x - xx) * (x - xx) + (y - yy) * (y - yy));
+
+	cout << "Rasstyanie = " << l << endl;
+	return 0;
+}
