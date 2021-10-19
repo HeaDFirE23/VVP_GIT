2438e565277dbfb8000932f23565daa750ff219f
Aleksandr <skurixxx03@gmail.com>
2021-10-19 04:37:13 +0300

    Лаб 5.3

---
+++ Lab5.3/Lab5.3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <math.h>
+
+using namespace std;
+double a, b, c, ac, bc, pr;
+int main()
+{
+	cin >> a >> c >> b;
+	ac = abs(a - c);
+	bc = abs(b - c);
+	pr = ac * bc;
+
+	cout << "AC = " << ac << endl;
+	cout << "BC = " << bc << endl;
+	cout << "AC * BC = " << pr << endl;
+	return 0;
+}
