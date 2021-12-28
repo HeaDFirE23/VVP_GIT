77a85bc56309f41e8128a158ccc0d214006f7338
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2021-12-28 14:57:48 +0300

    Add files via upload

---
+++ Lab6.1(1).cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+using namespace std;
+
+int A, B, x;
+
+int main()
+{
+    cin >> A >> B;
+    x = A;
+    A = B;
+    B = x;
+    cout << A << ' ' << B << endl;
+    return 0;
+}
