5fedf130d43f07a6652f04f104f1002487e60828
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 21:58:01 +0300

    Add files via upload

---
+++ Lab9.3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int K = 145, N = 4;
+
+int main()
+{
+    K = K + N - 1;
+    if (K % 7 != 0)
+    {
+        cout << K % 7 << endl;
+    }
+    else
+    {
+        cout << '7' << endl;
+    }
+}
