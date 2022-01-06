ceb995e6688e6dc33445d9213b1676e92e6b76a3
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 20:51:30 +0300

    Add files via upload

---
+++ Lab 7.6.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+double A1 = 4, A2 = 3, B1 = -1, B2 = 2, C1 = 9, C2 = 3, x, y, k;
+
+int main()
+{
+	k = A1 / A2;
+	if (k >= 1)
+	{
+		y = (C2 * k - C1) / (B2 * k - B1);
+	}
+	else
+	{
+		y = (C1 / k - C2) / (B1 / k - B2);
+	}
+	x = (C1 - B1 * y) / A1;
+    cout << 'x = ' << x << endl;
+	cout << 'y = ' << y << endl;
+}
