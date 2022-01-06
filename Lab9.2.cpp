314094bb01dcb3ae1f045723b8b5c09560c5528a
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 21:57:53 +0300

    Add files via upload

---
+++ Lab9.2.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+using namespace std;
+int N = 285;
+
+int main()
+{
+    cout << N % 7 << endl;
+    if (N % 7 == 1)
+    {
+        cout << "Понедельник" << endl;
+    }
+    if (N % 7 == 2)
+    {
+        cout << "Вторник" << endl;
+    }
+    if (N % 7 == 3)
+    {
+        cout << "Среда" << endl;
+    }
+    if (N % 7 == 4)
+    {
+        cout << "Четверг" << endl;
+    }
+    if (N % 7 == 5)
+    {
+        cout << "Пятница" << endl;
+    }
+    if (N % 7 == 6)
+    {
+        cout << "Суббота" << endl;
+    }
+    if (N % 7 == 0)
+    {
+        cout << "Воскресенье" << endl;
+    }
+}
