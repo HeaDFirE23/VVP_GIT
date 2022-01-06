49e054be48803b7aea21541bdfb97513736d43ab
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:44:18 +0300

    Add files via upload

---
+++ Lab11.2.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+using namespace std;
+int main() 
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    if (a >= b && b >= c)
+        cout << a + b << endl;
+    else if (a >= b && b <= c)
+        cout << a + c << endl;
+    else if (b >= a && a >= c)
+        cout << b + a << endl;
+    else if (b >= a && a <= c)
+        cout << c + b << endl;
+    else if (c >= a && b <= a)
+        cout << c + a << endl;
+    else if (c >= a && b >= a)
+        cout << c + b << endl;
+    return 0;
+}
