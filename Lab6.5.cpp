9260ea134ceb01ee9c5b4104f5ac6ac356b96835
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 20:50:16 +0300

    Add files via upload

---
+++ Lab6.5.cpp
@@ -0,0 +1,15 @@
+﻿
+#include <iostream>
+
+using namespace std;
+
+int x, y, z;
+
+int main()
+{
+    cin >> x;
+    z = (x - 3) * (x - 3) * (x - 3);
+    y = 4 * (z * z) - 7 * z + 2;
+    cout << y << endl;
+    return 0;
+}
