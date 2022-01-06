09ca364895ab68f31203f728e1a87fcdd7383dd2
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:43:40 +0300

    Add files via upload

---
+++ Lab10.7.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a{ 3 }, b{ 4 }, c{ 5 };
+    cout << "\n\n a b c = " << a << " " << b << " " << c;
+    bool f{ false };
+    if ((a + b > c) && (a + c > b) && (b + c > a))
+    {
+        f = true;
+    }
+    if (f == true)
+    {
+        cout << "\nИстина";
+    }
+    else
+    {
+        cout << "\nЛожь";
+    }
+}
