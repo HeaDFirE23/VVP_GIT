216d37a04aed394df758a04013dd8f64666fc7a1
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:44:58 +0300

    Add files via upload

---
+++ Lab11.6.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    int a;
+    cin >> a;
+    if (a / 2 == 0) 
+    {
+        cout << "четное";
+
+    }
+    else 
+    {
+        cout << "нечетное ";
+    }
+    if (a / 100 >= 1) 
+    {
+        cout << "трехзначное число";
+
+    }
+    else 
+    {
+        if (a / 10 >= 1) 
+        {
+            cout << "двузначное число";
+
+        }
+        else 
+        {
+            cout << "однозначное число";
+        }
+    }
+
+    return 0;
+}
