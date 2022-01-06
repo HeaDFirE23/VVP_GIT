d0607ae6233eb5bd00359c5d91fb7de2053f541a
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 22:39:24 +0300

    Add files via upload

---
+++ Lab9.4.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+using namespace std;
+int A = 5, B = 5, C = 2, k, n;
+
+int main()
+{
+    k = (A * B) / (C * C);
+    n = (A * B) - (C * C * k);
+    cout << "Количество квадратов = " << k << endl;
+    cout << "Незанятая площадь = " << n << endl;
+    cout << A * B << endl;
+}
