d140943102c4b58f9e1ab21ce47ebf58ec7a70b7
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 22:39:41 +0300

    Add files via upload

---
+++ Lab10.1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int A, B;
+
+int main()
+{
+    cin >> A >> B;
+    cout << "A > 2 и B <= 3" << endl;
+    if (A > 2 && B <= 3)
+    {
+        cout << "Справедливо" << endl;
+    }
+    else
+    {
+        cout << "Не Справедливо" << endl;
+    }
+}
