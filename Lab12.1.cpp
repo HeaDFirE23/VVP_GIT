8ecd8709931ea4757f696f594496e5f3fc848c64
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:45:13 +0300

    Add files via upload

---
+++ Lab12.1.cpp
@@ -0,0 +1,181 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int m = 11, d = 12;
+	if (d == 1)
+	{
+		cout << "Pervoe " ;
+	}
+	if (d == 2)
+	{
+		cout << "Vtoroe " ;
+	}
+	if (d == 3)
+	{
+		cout << "Tretie " ;
+	}
+	if (d == 4)
+	{
+		cout << "Chetviortoe " ;
+	}
+	if (d == 5)
+	{
+		cout << "Pyatoe " ;
+	}
+	if (d == 6)
+	{
+		cout << "Shestoye " ;
+	}
+	if (d == 7)
+	{
+		cout << "Sedimoe " ;
+	}
+	if (d == 8)
+	{
+		cout << "Vosimoe " ;
+	}
+	if (d == 9)
+	{
+		cout << "Deviatoe " ;
+	}
+	if (d == 10)
+	{
+		cout << "Desiatoe " ;
+	}
+	if (d == 11)
+	{
+		cout << "Odinadsatoe " ;
+	}
+	if (d == 12)
+	{
+		cout << "Dvenadsatoe " ;
+	}
+	if (d == 13)
+	{
+		cout << "Trenadsatoe " ;
+	}
+	if (d == 14)
+	{
+		cout << "Chetirnadsatoe " ;
+	}
+	if (d == 15)
+	{
+		cout << "Patnadsatoe " ;
+	}
+	if (d == 16)
+	{
+		cout << "Shesnadsatoe " ;
+	}
+	if (d == 17)
+	{
+		cout << "Semnadsatoe " ;
+	}
+	if (d == 18)
+	{
+		cout << "Vosemnadsatoe " ;
+	}
+	if (d == 19)
+	{
+		cout << "Devyatnadsatoe " ;
+	}
+	if (d == 20)
+	{
+		cout << "Dvadsatoe " ;
+	}
+	if (d == 21)
+	{
+		cout << "Dvadsat Pervoe " ;
+	}
+	if (d == 22)
+	{
+		cout << "Dvadsat Vtoroe " ;
+	}
+	if (d == 23)
+	{
+		cout << "Dvadsat Tretie " ;
+	}
+	if (d == 24)
+	{
+		cout << "Dvadsat Chetviortoe " ;
+	}
+	if (d == 25)
+	{
+		cout << "Dvadsat Shestoye " ;
+	}
+	if (d == 26)
+	{
+		cout << "Dvadsat Pervoe " ;
+	}
+	if (d == 27)
+	{
+		cout << "Dvadsat Sedimoe " ;
+	}
+	if (d == 28)
+	{
+		cout << "Dvadsat Vosimoe " ;
+	}
+	if (d == 29)
+	{
+		cout << "Dvadsat Deviatoe " ;
+	}
+	if (d == 30)
+	{
+		cout << "Tridsatoe " ;
+	}
+	if (d == 31)
+	{
+		cout << "Tridsatoe Pervoe " ;
+	}
+
+	if (m == 1)
+	{
+		cout << "Yanvarya" ;
+	}
+	if (m == 2)
+	{
+		cout << "Fevralya" ;
+	}
+	if (m == 3)
+	{
+		cout << "Marta" ;
+	}
+	if (m == 4)
+	{
+		cout << "Aprelya" ;
+	}
+	if (m == 5)
+	{
+		cout << "Maya" ;
+	}
+	if (m == 6)
+	{
+		cout << "Iyunya" ;
+	}
+	if (m == 7)
+	{
+		cout << "Iyulya" ;
+	}
+	if (m == 8)
+	{
+		cout << "Avgusta" ;
+	}
+	if (m == 9)
+	{
+		cout << "Sentyabrya" ;
+	}
+	if (m == 10)
+	{
+		cout << "Oktyabrya" ;
+	}
+	if (m == 11)
+	{
+		cout << "Noyabrya" ;
+	}
+	if (m == 12)
+	{
+		cout << "Dekabrya" ;
+	}
+}
