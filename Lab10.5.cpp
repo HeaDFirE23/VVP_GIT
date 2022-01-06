796c29dcb6230899efac83cea99e698748131386
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:43:06 +0300

    Add files via upload

---
+++ Lab10.5.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int x = 7557;
+
+int main()
+{
+    cout << "Данное число читается одинаково слева направо и справа налево" << endl;
+    if (x / 1000 == x % 10 && ((x / 100) % 10) == ((x / 10) % 10))
+    {
+        cout << "Истинно" << endl;
+    }
+    else
+    {
+        cout << "Ложно" << endl;
+    }
+}
