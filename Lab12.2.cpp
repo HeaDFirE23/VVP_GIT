98cd70373ca2fdc7db4d5d281f710da7d4d8e109
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:45:25 +0300

    Add files via upload

---
+++ Lab12.2.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    char C{ 'В' };
+    int N{ 1 }, K{ (int)(C) };
+    switch (K)
+    {
+    case -47:
+        K = 1;
+        break;
+    case -57:
+        K = 2;
+        break;
+    case -34:
+        K = 3;
+        break;
+    case -62:
+        K = 4;
+        break;
+    }
+    switch (N)
+    {
+    case 1:
+        ++K;
+        break;
+    case -1:
+        --K;
+        break;
+    }
+    if (K == 5)
+    {
+        K = 1;
+    }
+    if (K == 0)
+    {
+        K = 4;
+    }
+    switch (K)
+    {
+    case 1:
+        std::cout << "\n\nCевер";
+        break;
+    case 2:
+        std::cout << "\n\nЗапад";
+        break;
+    case 3:
+        std::cout << "\n\nЮг";
+        break;
+    case 4:
+        std::cout << "\n\nВосток";
+        break;
+    }
+}
