803033ca0ea7f3adb8684e810248f0cd9b4e9051
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:44:49 +0300

    Add files via upload

---
+++ Lab11.5.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main() 
+{
+    double a;
+    cin >> a;
+
+    if (a > 0) 
+    {
+        cout << "положительное ";
+
+    }
+    if (a < 0) 
+    {
+        cout << "отрицательное ";
+
+    }
+    if (a == 0) 
+    {
+        cout << "нулевое число ";
+    }
+    else 
+    {
+        if (a / 2 == 0) 
+        {//костыль на нулевое число 
+            cout << "нечетное число ";
+
+        }
+
+        else 
+        {
+            cout << "четное число ";
+        }
+    }
+
+    return 0;
+}
