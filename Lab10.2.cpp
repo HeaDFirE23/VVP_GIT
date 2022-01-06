26785f9ebc2b94880493e2071797a2147f170ee0
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 22:39:57 +0300

    Add files via upload

---
+++ Lab10.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int A, B, C;
+
+int main()
+{
+    cin >> A >> B >> C;
+    cout << "A < B < C" << endl;
+    if (A < B && B < C)
+    {
+        cout << "Справедливо" << endl;
+    }
+    else
+    {
+        cout << "Не Справедливо" << endl;
+    }
+}
