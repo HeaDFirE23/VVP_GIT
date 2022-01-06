6c204a6840acd5414fd0d36c29342cb9628fe434
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:44:26 +0300

    Add files via upload

---
+++ Lab11.3.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c, s;
+    cin >> a >> b >> c;
+    if (b > c) {
+        s = c - a;
+        cout << c << " " << s;
+    }
+    else if (b < c) {
+        s = b - a;
+        cout << b << " " << s;
+    }
+
+    return 0;
+}
