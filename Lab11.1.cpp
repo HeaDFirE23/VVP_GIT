1b4d7f546d8a8e18128789ea57e93d26ec5dc022
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-07 02:44:05 +0300

    Add files via upload

---
+++ Lab11.1.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double a, b;
+    cin >> a >> b;
+    if (a > b) {
+        b = a;
+        cout << a << " " << b;
+    }
+    else {
+        if (a < b) {
+            a = b;
+            cout << a << " " << b;
+        }
+        else {
+            if (a = b) {
+                a = 0;
+                b = 0;
+                cout << a << " " << b;
+            }
+        }
+    }
+
+    return 0;
+}
