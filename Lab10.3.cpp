cd6e1608618f897454f7a1c1c29be00938bd1f9e
HeaDFirE23 <91217827+HeaDFirE23@users.noreply.github.com>
2022-01-06 22:40:12 +0300

    Add files via upload

---
+++ Lab10.3.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+int x = 22;
+
+int main()
+{
+    cout << "Данное число является четным двузначным" << endl;
+    if (x % 2 == 0 && x / 10 > 1)
+    {
+        cout << "Справедливо" << endl;
+    }
+    else
+    {
+        cout << "Не Справедливо" << endl;
+    }
+}
